ccf44acbaac30e8b63e471d64457c7fc4815462e
qinrenchuan <568301493@qq.com>
2019-11-09 18:16:58 +0800

    init.....

---
+++ platform-refund/src/main/java/com/tdt/platform/refund/PlatformRefundApplication.java
@@ -0,0 +1,13 @@
+package com.tdt.platform.refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformRefundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformRefundApplication.class, args);
+    }
+
+}
---
+++ platform-refund/src/test/java/com/tdt/platform/refund/PlatformRefundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdt.platform.refund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformRefundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
